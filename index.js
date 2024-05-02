8543dabafe74a72444900120dd6c4261550c04b3
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-05-03 01:39:29 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,187 @@
+const { eventSource, event_types, getCurrentChatId } = SillyTavern.getContext();
+import { debounce } from '../../../utils.js';
+
+// Source: https://github.com/bartaz/sandbox.js/blob/master/jquery.highlight.js
+if (!jQuery.fn.highlight) {
+    console.log('Patching jQuery highlight');
+    jQuery.extend({
+        highlight: function (node, re, nodeName, className) {
+            if (node.nodeType === 3) {
+                var match = node.data.match(re);
+                if (match) {
+                    var highlight = document.createElement(nodeName || 'span');
+                    highlight.className = className || 'highlight';
+                    var wordNode = node.splitText(match.index);
+                    wordNode.splitText(match[0].length);
+                    var wordClone = wordNode.cloneNode(true);
+                    highlight.appendChild(wordClone);
+                    wordNode.parentNode.replaceChild(highlight, wordNode);
+                    return 1; //skip added node in parent
+                }
+            } else if ((node.nodeType === 1 && node.childNodes) && // only element nodes that have children
+                !/(script|style)/i.test(node.tagName) && // ignore script and style nodes
+                !(node.tagName === nodeName.toUpperCase() && node.className === className)) { // skip if already highlighted
+                for (var i = 0; i < node.childNodes.length; i++) {
+                    i += jQuery.highlight(node.childNodes[i], re, nodeName, className);
+                }
+            }
+            return 0;
+        }
+    });
+
+    jQuery.fn.unhighlight = function (options) {
+        var settings = { className: 'highlight', element: 'span' };
+        jQuery.extend(settings, options);
+
+        return this.find(settings.element + "." + settings.className).each(function () {
+            var parent = this.parentNode;
+            parent.replaceChild(this.firstChild, this);
+            parent.normalize();
+        }).end();
+    };
+
+    jQuery.fn.highlight = function (words, options) {
+        var settings = { className: 'highlight', element: 'span', caseSensitive: false, wordsOnly: false };
+        jQuery.extend(settings, options);
+
+        if (words.constructor === String) {
+            words = [words];
+        }
+        words = jQuery.grep(words, function (word, i) {
+            return word != '';
+        });
+        words = jQuery.map(words, function (word, i) {
+            return word.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+        });
+        if (words.length == 0) { return this; };
+
+        var flag = settings.caseSensitive ? "" : "i";
+        var pattern = "(" + words.join("|") + ")";
+        if (settings.wordsOnly) {
+            pattern = "\\b" + pattern + "\\b";
+        }
+        var re = new RegExp(pattern, flag);
+
+        return this.each(function () {
+            jQuery.highlight(this, re, settings.element, settings.className);
+        });
+    };
+}
+
+const sheld = document.getElementById('sheld');
+const chat = document.getElementById('chat');
+const topBar = document.createElement('div');
+const chatName = document.createElement('div');
+const searchInput = document.createElement('input');
+
+const icons = [
+    {
+        id: 'extensionTopBarChatManager',
+        icon: 'fa-solid fa-address-book',
+        position: 'left',
+        title: 'View chat files',
+        onClick: onChatManagerClick,
+    },
+    {
+        id: 'extensionTopBarCloseChat',
+        icon: 'fa-solid fa-times',
+        position: 'right',
+        title: 'Close chat',
+        onClick: onCloseChatClick,
+    }
+];
+
+function onChatManagerClick() {
+    document.getElementById('option_select_chat')?.click();
+}
+
+function onCloseChatClick() {
+    document.getElementById('option_close_chat')?.click();
+}
+
+function patchSheldIfNeeded() {
+    // Fun fact: sheld is a typo. It should be shell.
+    // It was fixed in OG TAI long ago, but we still have it here.
+    if (!sheld) {
+        console.error('Sheld not found. Did you finally rename it?');
+        return;
+    }
+
+    const computedStyle = getComputedStyle(sheld);
+    // Alert: We're not in a version that switched sheld to flex yet.
+    if (computedStyle.display === 'grid') {
+        sheld.classList.add('flexPatch');
+    }
+}
+
+function setChatName(name) {
+    const isNotInChat = !name;
+    chatName.innerText = name || 'No chat selected';
+    icons.forEach(icon => {
+        const iconElement = document.getElementById(icon.id);
+        if (iconElement) {
+            iconElement.classList.toggle('disabled', isNotInChat);
+        }
+    });
+}
+
+/**
+ * Highlight search query in chat messages
+ * @param {string} query Search query
+ * @returns {void}
+ */
+function searchInChat(query) {
+    const options = { element: 'mark', className: 'highlight' };
+    const messages = jQuery(chat).find('.mes_text');
+    messages.unhighlight(options);
+    if (!query) {
+        return;
+    }
+    const splitQuery = query.split(/\s|\b/);
+    messages.highlight(splitQuery, options);
+}
+
+const searchDebounced = debounce((x) => searchInChat(x), 500);
+
+function addTopBar() {
+    chatName.id = 'extensionTopBarChatName';
+    topBar.id = 'extensionTopBar';
+    searchInput.id = 'extensionTopBarSearchInput';
+    searchInput.placeholder = 'Search...';
+    searchInput.classList.add('text_pole');
+    searchInput.type = 'search';
+    searchInput.addEventListener('input', () => searchDebounced(searchInput.value.trim()));
+    topBar.appendChild(chatName);
+    topBar.appendChild(searchInput);
+    sheld.insertBefore(topBar, chat);
+}
+
+function addIcons() {
+    icons.forEach(icon => {
+        const iconElement = document.createElement('i');
+        iconElement.id = icon.id;
+        iconElement.className = icon.icon;
+        iconElement.title = icon.title;
+        iconElement.classList.add('right_menu_button');
+        iconElement.addEventListener('click', () => {
+            if (iconElement.classList.contains('disabled')) {
+                return;
+            }
+            icon.onClick();
+        });
+        if (icon.position === 'left') {
+            topBar.insertBefore(iconElement, chatName);
+            return;
+        }
+        if (icon.position === 'right') {
+            topBar.appendChild(iconElement);
+            return;
+        }
+    });
+}
+
+patchSheldIfNeeded();
+addTopBar();
+addIcons();
+setChatName(getCurrentChatId());
+eventSource.on(event_types.CHAT_CHANGED, setChatName);
